a587874ec72f6e1adec3eeac0ab6354d31188bdc
seveibar <seveibar@gmail.com>
2025-12-23 16:02:30 -0800

    update snapshots

--- tests/bugs/bugreport27-dd3734.test.ts
+++ tests/bugs/bugreport27-dd3734.test.ts
@@ -9,6 +9,15 @@ import { getLastStepSvg } from "../fixtures/getLastStepSvg"
 const srj = bugReport.simple_route_json as SimpleRouteJson
 
 test("bugreport27-dd3734", () => {
+  const portPointWinningHyperParameters = {
+    NODE_PF_FACTOR: 10000,
+    FORCE_OFF_BOARD_FREQUENCY: 0.3,
+    CENTER_OFFSET_DIST_PENALTY_FACTOR: 1,
+    FORCE_CENTER_FIRST: true,
+    SHUFFLE_SEED: 2139,
+    MIN_ALLOWED_BOARD_SCORE: -1,
+  }
+
   const solver = new AssignableAutoroutingPipeline2(srj)
 
   // // solve until the high density route solver, take a snapshot of the
