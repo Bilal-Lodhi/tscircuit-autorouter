b3e989bf7e964388b86fa630c1cfd9d06a50db50
0hm☘️ <git8ankanroy@gmail.com>
2025-12-25 02:49:57 +0530

    WIP

--- examples/benchmark/dataset01/e2e3.fixture.tsx
+++ examples/benchmark/dataset01/e2e3.fixture.tsx
@@ -1,1 +1,1 @@
-export * from "examples/legacy/end-to-end/e2e3.fixture"
+export { default } from "examples/legacy/end-to-end/e2e3.fixture"
--- examples/benchmark/dataset01/keyboard04.fixture.tsx
+++ examples/benchmark/dataset01/keyboard04.fixture.tsx
@@ -1,1 +1,1 @@
-export * from "examples/legacy/keyboards/keyboard4.fixture"
+export { default } from "examples/legacy/keyboards/keyboard4.fixture"
