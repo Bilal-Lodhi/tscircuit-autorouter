b093db11e2743372a1b447f1107ee602d75b7c4f
seveibar <seveibar@gmail.com>
2026-01-02 12:36:58 -0800

    fix types

--- fixtures/jumper-prepattern/prepattern02.fixture.tsx
+++ fixtures/jumper-prepattern/prepattern02.fixture.tsx
@@ -9,7 +9,7 @@ export default () => {
       nodeWithPortPoints: input.nodeWithPortPoints as any,
       colorMap: input.colorMap,
       traceWidth: input.traceWidth,
-      hyperParameters: input.hyperParameters,
+      hyperParameters: input.hyperParameters as any,
     })
   }, [])
 
