9fd12053402dea3ff7ce7710eff1af72d71d87f3
seveibar <seveibar@gmail.com>
2026-01-03 14:12:06 -0800

    wip allow node cycles

--- lib/solvers/PortPointPathingSolver/PortPointPathingSolver.ts
+++ lib/solvers/PortPointPathingSolver/PortPointPathingSolver.ts
@@ -1472,9 +1472,9 @@ export class PortPointPathingSolver extends BaseSolver {
       if (!nextNodeId) continue
 
       // HACK: Disable node cycles because stitch solver doesn't handle them
-      if (this.isNodeInPathChain(currentCandidate, nextNodeId)) continue
-      // if (currentCandidate.currentNodeId === nextNodeId) continue
-      // if (currentCandidate.prevCandidate?.currentNodeId === nextNodeId) continue
+      // if (this.isNodeInPathChain(currentCandidate, nextNodeId)) continue
+      if (currentCandidate.currentNodeId === nextNodeId) continue
+      if (currentCandidate.prevCandidate?.currentNodeId === nextNodeId) continue
 
       const throughNodeId =
         "throughNodeId" in portPoint
