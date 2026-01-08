44c492bbdb85a51d4795b5046f53a15244f3567c
seveibar <seveibar@gmail.com>
2026-01-08 15:05:39 -0800

    enable vertical

--- lib/solvers/JumperPrepatternSolver/HyperJumperPrepatternSolver2.ts
+++ lib/solvers/JumperPrepatternSolver/HyperJumperPrepatternSolver2.ts
@@ -139,7 +139,7 @@ export class HyperJumperPrepatternSolver2 extends HyperParameterSupervisorSolver
       {
         name: "orientation",
         possibleValues: [
-          // { ORIENTATION: "vertical" },
+          { ORIENTATION: "vertical" },
           { ORIENTATION: "horizontal" },
         ],
       },
