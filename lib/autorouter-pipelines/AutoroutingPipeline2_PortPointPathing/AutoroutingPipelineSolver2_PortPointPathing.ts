4fd282708de36bc42641df40658d429f20025583
0hm☘️ <109351887+0hmX@users.noreply.github.com>
2026-01-21 00:22:39 +0530

    Update lib/autorouter-pipelines/AutoroutingPipeline2_PortPointPathing/AutoroutingPipelineSolver2_PortPointPathing.ts
    Co-authored-by: Severin Ibarluzea <seveibar@gmail.com>

--- lib/autorouter-pipelines/AutoroutingPipeline2_PortPointPathing/AutoroutingPipelineSolver2_PortPointPathing.ts
+++ lib/autorouter-pipelines/AutoroutingPipeline2_PortPointPathing/AutoroutingPipelineSolver2_PortPointPathing.ts
@@ -289,7 +289,7 @@ export class AutoroutingPipelineSolver2_PortPointPathing extends BaseSolver {
       (cms) => {
         const portPointSolver = cms.portPointPathingSolver!
         const effort = cms.effort ?? 1
-        const effortRatio = Math.min(Math.max((effort - 1) / 99, 0), 1)
+        const effortFractionOf100 = Math.min(Math.max((effort - 1) / 99, 0), 1)
         const effortMultiplier = 1 - effortRatio * 0.8
         const minAcceptablePf = 0.16 * effortMultiplier
         const maxAcceptablePf = 0.24 * effortMultiplier
