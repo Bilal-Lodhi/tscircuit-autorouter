14681349d1fd00d747bac6b3fac935b1769232ca
0hm☘️ <git8ankanroy@gmail.com>
2026-02-24 19:15:19 +0530

    Refactor areAllRegionPortsBlocked to improve port blockage logic and return value

--- lib/solvers/HopCheckSolver/areAllRegionPortsBlocked.ts
+++ lib/solvers/HopCheckSolver/areAllRegionPortsBlocked.ts
@@ -1,13 +1,19 @@
 import { TypedRegionPort } from "./HopCheckSolver"
 
 export const areAllRegionPortsBlocked = (regionPorts: TypedRegionPort[]) => {
+  if (regionPorts.length === 0) return false
+
+  let result = true
   for (const port of regionPorts) {
     const neighborRegions = [port.region1, port.region2]
+    let isPortBlocked = false
     for (const neighborRegion of neighborRegions) {
-      if (!neighborRegion.d._containsObstacle) {
-        return false
+      if (neighborRegion.d._containsObstacle) {
+        isPortBlocked = true
+        break
       }
     }
+    result = result && isPortBlocked
   }
-  return true
+  return result
 }
