b90ceb39469ad9518dc5a41ee2312c1daf56239c
0hm☘️ <git8ankanroy@gmail.com>
2026-04-15 22:46:54 +0530

    new: add bug-report-49-8536f4

---
+++ fixtures/bug-reports/bugreport49-8536f4/bugreport49-8536f4.fixture.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
+import bugReportJson from "./bugreport49-8536f4.json"
+
+export default () => {
+  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
+}
