7ec3bb88c2a52abe1a2574e9a25ecc668db73883
Bilal-Lodhi <braza4715@gmail.com>
2026-05-12 23:52:45 +0500

    fix: enforce type erasure for CI compatibility

--- lib/autorouter-pipelines/AutoroutingPipeline5_HdCache/Pipeline5HdCacheHighDensitySolver.ts
+++ lib/autorouter-pipelines/AutoroutingPipeline5_HdCache/Pipeline5HdCacheHighDensitySolver.ts
@@ -1,7 +1,5 @@
-type ConnectivityMap =
-  any
-type GraphicsObject =
-  any
+type ConnectivityMap = any
+type GraphicsObject = any
 import type { CapacityMeshNodeId } from "lib/types/capacity-mesh-types"
 import { mergeRouteSegments } from "lib/utils/mergeRouteSegments"
 import { BaseSolver, type PendingEffect } from "../../solvers/BaseSolver"
