6bde69496b4432557dd050931c42a585a5a85b63
Bilal-Lodhi <braza4715@gmail.com>
2026-05-12 23:40:51 +0500

    fix: resolve CI type-check and biome formatting errors

--- lib/autorouter-pipelines/AutoroutingPipeline5_HdCache/Pipeline5HdCacheHighDensitySolver.ts
+++ lib/autorouter-pipelines/AutoroutingPipeline5_HdCache/Pipeline5HdCacheHighDensitySolver.ts
@@ -1,5 +1,17 @@
-interface ConnectivityMap { getNetId: (pinId: string) => string | undefined }
-interface GraphicsObject { lines: any[], points: any[], rects: any[], circles: any[] }
+interface ConnectivityMap {
+getNetId: (pinId: string) => string | undefined
+netMap: Record<string, string[]>
+idToNetMap: Record<string, string>
+addConnections: (connections: any[]) => void
+getIdsConnectedToNet: (netId: string) => string[]
+[key: string]: any 
+}
+interface GraphicsObject {
+lines: any[]
+points: any[]
+rects: any[]
+circles: any[]
+}
 import type { CapacityMeshNodeId } from "lib/types/capacity-mesh-types"
 import { mergeRouteSegments } from "lib/utils/mergeRouteSegments"
 import { BaseSolver, type PendingEffect } from "../../solvers/BaseSolver"
